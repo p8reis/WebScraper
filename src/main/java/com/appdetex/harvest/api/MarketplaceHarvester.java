e234d83e73cbb9510032a8e6d44049c14d6d6f12
Joana Camacho <joana.camacho@appdetex.com>
2022-11-07 15:17:53 +0000

    master

--- src/main/java/com/appdetex/harvest/api/MarketplaceHarvester.java
+++
@@ -1,8 +0,0 @@
-package com.appdetex.harvest.api;
-
-import java.util.List;
-
-public interface MarketplaceHarvester {
-
-        List<MarketplaceDetection> parseTarget(String term, int numItems);
-}
