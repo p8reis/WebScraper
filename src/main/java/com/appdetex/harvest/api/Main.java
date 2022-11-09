32366c0db07f24b2d603952efb4794d061f607b7
Pedro Temtem <pedro.temtem@appdetex.com>
2022-11-09 17:48:09 +0000

    harvesters merged

--- src/main/java/com/appdetex/harvest/api/Main.java
+++ src/main/java/com/appdetex/harvest/api/Main.java
@@ -1,12 +1,16 @@
 package com.appdetex.harvest.api;
 
 import com.appdetex.harvest.marketplace.AbstractHarvester;
+import com.appdetex.harvest.marketplace.MercadoLivreBrHarvester;
 
 import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, HarvestException {
+        MercadoLivreBrHarvester mercadoLivreBrHarvester = new MercadoLivreBrHarvester();
+        mercadoLivreBrHarvester.parseTarget("jacuzzi", 10);
+
 
     }
 }
