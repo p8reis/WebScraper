ac0eb9a86d90d1001b3f706822e51541e2d0e8b9
Pedro Temtem <pedro.temtem@appdetex.com>
2022-11-10 10:31:52 +0000

    main with calls for harvester

--- src/main/java/com/appdetex/harvest/api/Main.java
+++ src/main/java/com/appdetex/harvest/api/Main.java
@@ -1,15 +1,30 @@
 package com.appdetex.harvest.api;
 
-import com.appdetex.harvest.marketplace.AbstractHarvester;
-import com.appdetex.harvest.marketplace.MercadoLivreBrHarvester;
+import com.appdetex.harvest.marketplace.*;
 
 import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) throws IOException, HarvestException {
+
+        int numItems = 10;
+        String term = "jacuzzi";
+
         MercadoLivreBrHarvester mercadoLivreBrHarvester = new MercadoLivreBrHarvester();
-        mercadoLivreBrHarvester.parseTarget("jacuzzi", 10);
+        mercadoLivreBrHarvester.parseTarget(term,numItems);
+
+        AmazonEsHarvester amazonEsHarvester =new AmazonEsHarvester();
+        amazonEsHarvester.parseTarget(term, numItems);
+
+        AmazonUsHarvester amazonUsHarvester =new AmazonUsHarvester();
+        amazonUsHarvester.parseTarget(term, numItems);
+
+        DecathlonPtHarvester decathlonPtHarvester = new DecathlonPtHarvester();
+        decathlonPtHarvester.parseTarget(term, numItems);
+
+
+
 
 
     }
