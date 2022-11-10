07e2b75cc3efa4cb2aa9c3b14ea393d26f1bbcfc
Pedro Reis <pedro.reis@appdetex.com>
2022-11-10 11:42:06 +0000

    updates Main.java

--- src/main/java/com/appdetex/harvest/api/Main.java
+++ src/main/java/com/appdetex/harvest/api/Main.java
@@ -10,22 +10,14 @@ public class Main {
         int numItems = 10;
         String term = "jacuzzi";
 
-        System.out.println("Step1");
-
         MercadoLivreBrHarvester mercadoLivreBrHarvester = new MercadoLivreBrHarvester();
         mercadoLivreBrHarvester.parseTarget(term,numItems);
-
-        System.out.println("Step2");
 
         AmazonEsHarvester amazonEsHarvester = new AmazonEsHarvester();
         amazonEsHarvester.parseTarget(term, numItems);
 
-        System.out.println("Step3");
-
         AmazonUsHarvester amazonUsHarvester = new AmazonUsHarvester();
         amazonUsHarvester.parseTarget(term, numItems);
-
-        System.out.println("Step4");
 
         DecathlonPtHarvester decathlonPtHarvester = new DecathlonPtHarvester();
         decathlonPtHarvester.parseTarget(term, numItems);
