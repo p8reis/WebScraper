74aed9a920d0e1618d91fcb5168b109b64316632
Joana Camacho <joana.camacho@appdetex.com>
2022-12-05 15:54:35 +0000

    adding brand_tracks table to mysql database

--- marketplace_harvest.sql
+++ marketplace_harvest.sql
@@ -60,11 +60,18 @@ CREATE TABLE IF NOT EXISTS `marketplace_harvest`.`marketplace_detections` (
   `status` VARCHAR(500) NULL DEFAULT 'open',
   `state` VARCHAR(500) NULL DEFAULT 'new',
   `reason_code` VARCHAR(500) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  `accounts_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `accounts_id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `url_UNIQUE` (`url` ASC) VISIBLE,
   INDEX `fk_marketplace_detections_status1_idx` (`status` ASC) VISIBLE,
-  INDEX `fk_marketplace_detections_state1_idx` (`state` ASC) VISIBLE)
+  INDEX `fk_marketplace_detections_state1_idx` (`state` ASC) VISIBLE,
+  INDEX `fk_marketplace_detections_accounts1_idx` (`accounts_id` ASC) VISIBLE,
+  CONSTRAINT `fk_marketplace_detections_accounts1`
+    FOREIGN KEY (`accounts_id`)
+    REFERENCES `marketplace_harvest`.`accounts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 AUTO_INCREMENT = 31
 DEFAULT CHARACTER SET = utf8mb3;
@@ -79,17 +86,38 @@ CREATE TABLE IF NOT EXISTS `marketplace_harvest`.`audit` (
   `parameter` ENUM('state', 'status', 'reason_code') NOT NULL,
   `marketplace_detections_id` INT UNSIGNED NOT NULL,
   `analysts_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
+  `analysts_id1` INT NOT NULL,
+  PRIMARY KEY (`id`, `analysts_id1`),
   INDEX `fk_audit_changes_marketplace_detections1_idx` (`marketplace_detections_id` ASC) VISIBLE,
-  INDEX `fk_audit_changes_analysts1_idx` (`analysts_id` ASC) VISIBLE,
-  CONSTRAINT `fk_audit_changes_analysts1`
-    FOREIGN KEY (`analysts_id`)
-    REFERENCES `marketplace_harvest`.`analysts` (`id`),
+  INDEX `fk_audit_analysts1_idx` (`analysts_id1` ASC) VISIBLE,
   CONSTRAINT `fk_audit_changes_marketplace_detections1`
     FOREIGN KEY (`marketplace_detections_id`)
-    REFERENCES `marketplace_harvest`.`marketplace_detections` (`id`))
+    REFERENCES `marketplace_harvest`.`marketplace_detections` (`id`),
+  CONSTRAINT `fk_audit_analysts1`
+    FOREIGN KEY (`analysts_id1`)
+    REFERENCES `marketplace_harvest`.`analysts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 AUTO_INCREMENT = 8
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `marketplace_harvest`.`brand_tracks`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `marketplace_harvest`.`brand_tracks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `search_term` VARCHAR(45) NULL DEFAULT NULL,
+  `accounts_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `accounts_id`),
+  INDEX `fk_brand_tracks_accounts1_idx` (`accounts_id` ASC) VISIBLE,
+  CONSTRAINT `fk_brand_tracks_accounts1`
+    FOREIGN KEY (`accounts_id`)
+    REFERENCES `marketplace_harvest`.`accounts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
